678feb9e214a4534d80578b8bd3f61ef66c47279
ben@castanfamily.com <CH8yE6hmgiUvjD>
2016-05-30 12:38:42 +1000

    Updated and tested this basically works

--- BupSter.py
+++ BupSter.py
@@ -33,6 +33,36 @@ def do_rsync(rh, ru, rd, rf, ld):
     # before issuing the rsync command, I've been running a mkdir command
     # Without this, if the directory did not exist, rsync would fail.
     # If the directory exists, then the mkdir command does nothing.
-    # If you are copying the file to the remote directoy, the mkdir command can be passed by ssh
+    # If you are copying the file to the remote directory, the mkdir command can be passed by ssh
     mkdir_cmd = '/bin/mkdir -p %s' % localdir
+
+    # create the rsync command
+    rsync_cmd = '/usr/bin/rsync -va %s %s' % (remote, local)
+
+    # now we run the commands
+    # shell=True is used as the escaped characters would cause failures
+    p1 = subprocess.Popen(mkdir_cmd, shell=True).wait()
+    p2 = subprocess.Popen(rsync_cmd, shell =True).wait()
+    print""
+    return 0
+
+
+rh = "10.100.1.223"
+ru = "benc"
+rd = "/Volumes/DATA/iTunes Media/Music/X/"
+rf = "Live At The Stagedoor Tavern"
+ld = "/Data/Music/X/"
+
+
+print "Here we do a simple test with test.dat"
+do_rsync(rh, ru, rd, rf, ld)
+
+#rf = "this is a filename - with (stuff) in it.dat"
+
+
+#print "Here is a filename witha bit more character"
+#do_rsync(rh, ru, rd, rf, ld)
+
+
+exit()
 
