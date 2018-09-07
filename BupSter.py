92ef8f8922be4cb5307afb69340fef868587f43d
Ben Castan <bcastan@appen.com>
2018-09-08 08:30:26 +1000

    Changing the way this works to suit LF RSync

--- BupSter.py
+++ BupSter.py
@@ -10,7 +10,7 @@
 import subprocess
 import os
 import re
-import yaml
+
 import Utils
 
 
@@ -85,35 +85,24 @@ def do_rsync(rh, ru, rd, ld, sw, rf=None):
         print('Opps! something is wrong at the remote end')
 
 
-def get_options():
-    with open("docs/config.txt", 'r') as ymlfile:
-        # rh == remote host name or ip address
-        # ru == remote user name
-        # rd == remote directory full path should be used
-        # rf == remote file if you want to move only one file
-        # ld == local directory, full path should be used
-        # md == The media files to copy i.e. the directory to copy the files from.
-        cfg = yaml.load(ymlfile)
-        rh = cfg['remote']['host']
-        ru = cfg['remote']['user']
-        rd = cfg['remote']['directory']
-        rf = cfg['remote']['file']
-        ld = cfg['local']['directory']
-        sw = cfg['options']['switch']
-        md = cfg['remote']['Media']
 
-        return{'optrh':rh, 'optru':ru, 'optrd':rd, 'optrf':rf, 'optld':ld,'optsw':sw, 'optmd':md}
 
 
 if __name__ == '__main__':
     #show_header()
-    #options= get_options()
+    options= Utils.Configs.get_options()
     # Dirs to rsync
-    #dirs = (options['optmd'])
-    #for rrd in dirs:
+    dirs = options
+    for rrd in dirs:
+        print(options['optrh'], options['optru'], options['optrd'], options['optrf'], options['optld'], options['optsw'])
+
+
+
+
     #    do_rsync(options['optrh'], options['optru'], options['optrd'] + rrd, options['optrf'], options['optld'], options['optsw'])
         #do_rsync(options['optrh'], options['optru'], options['optrd'] + rrd, options['optld'],
         #         options['optsw'])
 
-        print("Month {}".format(Utils.Times.get_monthDecimal()))
-        print("Year: {}".format(Utils.Times.get_yearDecimal))
+        #print("Month {}".format(Utils.Dates.get_monthDecimal()))
+        # print("Year: {}".format(Utils.Dates.get_yearDecimal()))
+        # print("Day: {}".format(Utils.Dates.get_dayDecimal()))
--- Utils.py
+++ Utils.py
@@ -1,13 +1,40 @@
-from time import strftime, gmtime
 import datetime
+import yaml
 
+class Dates():
+    # Grab the cuurent date and return the Year as `2018` and the month as `09`
+    # print("Current year: ", datetime.date.today().strftime("%Y"))
+    # print("Current Month: ", datetime.date.today().strftime("%m"))
+    # year = datetime.date.today().strftime("%Y")
+    # month = datetime.date.today().strftime("%m")
 
-class Times():
+    def get_dayDecimal():
+        return datetime.date.today().strftime("%d")
+
     def get_monthDecimal():
-        # Returns the current month as a decimal number i.e. 09
-        return strftime("%m")
+        return datetime.date.today().strftime("%m")
 
-    @property
     def get_yearDecimal():
-        # Returns the year with century as a decimal number
-        return strftime("%Y")
+        return datetime.date.today().strftime("%Y")
+
+
+class Configs():
+    @staticmethod
+    def get_options():
+        with open("docs/config.txt", 'r') as ymlfile:
+            # rh == remote host name or ip address
+            # ru == remote user name
+            # rd == remote directory full path should be used
+            # rf == remote file if you want to move only one file
+            # ld == local directory, full path should be used
+            # md == The media files to copy i.e. the directory to copy the files from.
+            cfg = yaml.load(ymlfile)
+            rh = cfg['remote']['host']
+            ru = cfg['remote']['user']
+            rd = cfg['remote']['directory']
+            rf = cfg['remote']['file']
+            ld = cfg['local']['directory']
+            sw = cfg['options']['switch']
+            md = cfg['remote']['Media']
+
+            return{'optrh':rh, 'optru':ru, 'optrd':rd, 'optrf':rf, 'optld':ld,'optsw':sw, 'optmd':md}
